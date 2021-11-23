674d717c048f0e375a1c0fac1f0176a63f486a72
Akash Rao <rao28271@gmail.com>
2021-11-24 02:32:58 +0530

    working_blog

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 1)Students can post Blogs(Technical/Adventure/College Related) on the website & all the NMAMITian's have access to the Blogs.
 2)Students can get Info About all the New Events happenning in the NMAMIT all in one place and can register through the website.
 3)Students can upload their Resume in the website and build their Network & Alumni
+
+db.users.find().limit(1).sort({$natural:-1}).pretty()
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const Article = require('./models/article')
 const mongoose = require('mongoose')
-const app = express()
+var bodyParser = require("body-parser")
+const app = express();
 
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-mongoose.createConnection('mongodb://localhost/blog').asPromise();
+mongoose.connect('mongodb://localhost:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// mongoose.connect('mongodb://localhost:27017/blog', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// mongoose.connect('mongodb://localhost/blog')
+
+var db = mongoose.connection;
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -11,14 +29,33 @@ app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
 app.set('views', './views')
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
     res.render('index')
 })
 
-app.post('', (req, res) => {
-    res.render('index')
+app.post("", (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "phno": phno,
+        "password": password
+    }
+    app.locals.name = name;
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+    return res.redirect('/home')
 })
 
 app.get('/signin', (req, res) => {
@@ -26,25 +63,51 @@ app.get('/signin', (req, res) => {
 })
 
 //use alternative of body-parser later
+app.get('/home', (req, res) => {
+    res.render('home', { error: false })
+})
 app.post('/home', (req, res) => {
-    res.render('home');
+    res.render('home')
 })
 
 app.get('/form', (req, res) => {
     res.render('form');
 })
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
-
-app.get('/blog', (req, res) => {
-    const articles = [{
-        title: 'test',
-        createdAt: new Date(),
-        description: 'test d'
-    }]
+//test 
+app.get('/blog', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('blog', { articles: articles })
 })
 
 
 
+app.get('/blog/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')
+    res.render('show', { article: article })
+})
+
+app.post('/blog', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.exampleFormControlTextarea1
+    })
+    try {
+        article = await article.save()
+        res.redirect(`/blog/`)
+    } catch (e) {
+        res.render('404')
+    }
+})
+
+
+
+app.get('/404', (req, res) => {
+    res.render('404')
+})
 
 app.listen(5000)
